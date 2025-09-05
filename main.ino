9f90b82691b161d0f5c9b222656533fdb37855e4
Sreyas Sabbani <sreyassabbani@gmail.com>
2025-09-05 06:24:56 -0400

    feat: wip with atomic write/read

--- main.ino
+++ main.ino
@@ -1,3 +1,5 @@
+#include <util/atomic.h>
+
 #define L1 7
 #define L2 6
 #define L3 5
@@ -22,17 +24,12 @@ enum LEDRecallState {
 enum AppState {
   Modify,
   Recall,
-  Start
-};
-
-enum _TransitionalAppState {
+  Start,
   TModify,
   TRecall,
-  TStart,
-  Junk
+  TStart
 };
 
-volatile int ratings[3] = {5, 2, 4};
 
 void setup()
 {
@@ -58,88 +55,142 @@ LEDRecallState lrs = Invalid; // invalid since appState != Recall
 AppState pastAppState = Start;
 LEDRecallState pastLrs = Invalid;
 
+// volatile int ratings[3] = {5, 2, 4};
+int rating_1 = 5;
+int rating_2 = 2;
+int rating_3 = 4;
+
 void loop()
 {
+  int curTime = millis();
+  int prevTime = 0;
+
   if (pastAppState != appState) {
-    Serial.print("App state: ");
+    Serial.print("App state has been changed to: ");
     Serial.println(appState);
     pastAppState = appState;
+
+    // we don't finish transition to appState.Modify until user selects an OS to rate
+    if (appState == TModify || appState == Start) reset();
   }
 
-  _TransitionalAppState tt = Junk;
   if (digitalRead(B1) == HIGH) {
-    lrs = MacOS;
-    if (appState != Modify) tt = TRecall;
-  } else if (digitalRead(B2) == HIGH) {
-  	lrs = Windows;
-    if (appState != Modify) tt = TRecall;
-  } else if (digitalRead(B3) == HIGH) {
-  	lrs = Linux;
-    if (appState != Modify) tt = TRecall;
-  } else if (digitalRead(BS) == HIGH) {
+    buttonPress(MacOS);
+    prevTime = curTime;
+  }
+  else if (digitalRead(B2) == HIGH) {
+    buttonPress(Windows);
+    prevTime = curTime;
+  }
+  else if (digitalRead(B3) == HIGH) {
+    buttonPress(Linux);
+    prevTime = curTime;
+  }
+  else if (digitalRead(BS) == HIGH) {
     // toggle
-    if (appState != Modify) {
-      tt = TModify;
-      Serial.println("hiii");
+    if (appState != Modify && appState != TModify) {
+      appState = TModify;
+      lrs = Invalid; // lrs is set after an OS is selected; then app will transition from appState.TModify to appState.Modify
+      Serial.println("Switched to appState.TModify mode");
+
+      delay(200); // delay after toggle to prevent rapid switches
     }
-    else appState = Start;
-    Serial.println("hihii");
+    else {
+      appState = TStart;
+      reset();
+      Serial.println("Switched to appState.TStart mode");
+
+      delay(200); // delay after toggle to prevent rapid switches
+      return;
+    }
   }
 
   if (lrs != pastLrs) {
-    Serial.print("lrs: ");
-    Serial.println(lrs);
+    Serial.print("LED recall state changed to: ");
+    if (lrs == 0) Serial.println("MacOS (value 0)");
+    if (lrs == 1) Serial.println("Windows (value 1)");
+    if (lrs == 2) Serial.println("Linux (value 2)");
+    if (lrs == 3) Serial.println("Invalid (value 3)");
     pastLrs = lrs;
   }
   
-  if (appState == Recall && tt != TRecall && tt != TModify) return;
-  else if (tt == TRecall) {
-    appState = (int) tt; // enums map to numbers starting with 0 index so we're safe
-
+  if (appState == Recall) return;
+  else if (appState == TRecall) {
+    int safe_rating_1;
+    int safe_rating_2;
+    int safe_rating_3;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      safe_rating_1 = rating_1;
+      safe_rating_2 = rating_2;
+      safe_rating_3 = rating_3;
+    }
     if (lrs == MacOS) {
       reset();
       delay(100);
-      animateTo(L5);
+      Serial.print("Displaying rating for MacOS as: ");
+      Serial.println(safe_rating_1);
+      animateTo(lrsFromRating(safe_rating_1));
     } else if(lrs == Linux) {
       reset();
       delay(100);
-      animateTo(L4);
+      Serial.print("Displaying rating for Linux as: ");
+      Serial.println(safe_rating_2);
+      animateTo(lrsFromRating(safe_rating_2));
     } else if (lrs == Windows) {
       reset();
       delay(100);
-      animateTo(L2);
+      Serial.print("Displaying rating for Windows as: ");
+      Serial.println(safe_rating_3);
+      animateTo(lrsFromRating(safe_rating_3));
     }
+    appState = Recall; // finish transition
   }
-
   
-  // while you're modifying, do
   if (appState == Modify) {
-    Serial.println("byeeee");
-    
-    int cur_rating = lrsFromRating(analogRead(POT) * (5.0 / 1023.0));
-
-    int past_rating = ratings[lrs];
-
-    Serial.print("animate to:");
-    Serial.println(past_rating);
-    Serial.print("animate from:");
+    int cur_rating = lrsFromRating((analogRead(POT) - 40) / 240 + 1);
     Serial.println(cur_rating);
 
-    animate(past_rating, cur_rating); // to, from
+    int past_rating;
+    if (lrs == MacOS) {
+      past_rating = rating_1;
+    } else if (lrs == Windows) {
+      past_rating = rating_2;
+    } else if (lrs == Linux) {
+      past_rating = rating_3;
+    }
+
+    if (past_rating != cur_rating) {
+      Serial.print("animate to: ");
+      Serial.println(cur_rating);
+      Serial.print("animate from: ");
+      Serial.println(past_rating);
+      animate(cur_rating, past_rating, true); // to, from
+    }
     
-    // persist rating
-    ratings[lrs] = cur_rating;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      if (lrs == MacOS) {
+        rating_1 = cur_rating;
+      } else if (lrs == Windows) {
+        rating_2 = cur_rating;
+      } else if (lrs == Linux) {
+        rating_3 = cur_rating;
+      }
+    }
   }
+}
 
-  // when turning on modify mode, wait until user picks which OS to rate
-  if (tt == TModify) {
-    reset();
-    delay(100);
-    Serial.println("HIHIHI");
-    appState = Modify; // finish transition
-    lrs = Invalid; // led user pick which OS to rate afterwards
+bool isTransitioning() {
+  return (int) appState > 2;
+}
+
+void buttonPress(LEDRecallState param_lrs) {
+  lrs = param_lrs;
+  if (appState != Modify && appState != TModify) appState = TRecall;
+  else if (appState == TModify) {
+    appState = Modify;
+    Serial.print("Switched to appState.Modify mode, selected the following to modify: ");
+    Serial.println(lrs);
   }
-
 }
 
 int lrsFromRating(int rating) {
@@ -154,11 +205,23 @@ void animateTo(int n) {
   }
 }
 
-void animate(int to, int from) {
-  for (int i = from; i >= to; --i) {
-  	digitalWrite(i, HIGH);
-    // Serial.println(1000 / (from - to));
-    delay((700 / (from - to)) + 200);
+void animate(int to, int from, bool fast) {
+  int time;
+  if (fast) time = 100;
+  else time = (700 / (from - to)) + 200;
+
+  if (to < from) {
+    for (int i = from; i >= to; --i) {
+      digitalWrite(i, HIGH);
+      // Serial.println(1000 / (from - to));
+      delay(time);
+    }
+  } else if (to > from) {
+    for (int i = from; i <= to; ++i) {
+      digitalWrite(i, LOW);
+      // Serial.println(1000 / (from - to));
+      delay(time);
+    }
   }
 }
 
